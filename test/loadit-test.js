4dbd29b31aa91f1bb95b82c77f963255e0533840
tommedema <tommedema@gmail.com>
2011-09-15 17:47:32 +0200

    added test

---
+++ test/loadit-test.js
@@ -0,0 +1,25 @@
+var vows    = require('vows'),
+    assert  = require('assert'),
+    loadit  = require('../loadit'),
+    dir     = __dirname + '/folder1',
+    regex   = /.js$/i;
+
+vows.describe('loadit callback').addBatch({
+    'Load property': {
+        topic: loadit.load,
+        
+        'is defined as a function' : function(loadFn) {
+            assert.deepEqual(typeof(loadFn), 'function');
+        },
+        
+        'when called' : {
+            'topic' : function(loadFn) {
+                loadFn(dir, regex, this.callback);
+            },
+            
+            'results in a list of .js files': function(err, files) {
+                
+            }
+        }
+    }
+}).export(module);
