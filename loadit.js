1c25a0828af1aaa815afe9fbd782bf7fb2717701
tommedema <tommedema@gmail.com>
2011-09-15 16:09:34 +0200

    initial structure

---
+++ loadit.js
@@ -0,0 +1,10 @@
+var load = exports.load = function(dir, regex, cb) {
+    
+    /* execute findit for this directory */
+    
+    /* perform regex for each file */
+    
+    /* require each validated file */
+    
+    /* call callback with err or all files that were loaded */
+};
